a385ec071f342302ea64347071f1eb058b9b1338
luis Torres <luistorresco@gmail.com>
2023-08-27 21:29:35 -0500

    logica php

--- phpmailer.php
+++ phpmailer.php
@@ -1,6 +1,7 @@
 <?php
 if($_SERVER['REQUEST_METHOD'] != 'POST' ){
     header("Location: index.html" );
+    exit;
 }
 
 require 'phpmailer/PHPMailer.php';
@@ -13,10 +14,27 @@ $email = $_POST['email'];
 $mensaje = $_POST['mensaje'];
 $telefono = $_POST['telefono']; 
 
-if( empty(trim($nombre)) ) $nombre = $nombre;
-if( empty(trim($email)) ) $email = $email;
-if( empty(trim($telefono)) ) $telefono = $telefono;
-if( empty(trim($mensaje)) ) $mensaje = $mensaje;
+
+
+// Validación del nombre
+if (preg_match('/[^\p{L}\s]+/u', $nombre)) {
+    echo "El campo de nombre no puede contener caracteres especiales.";
+    exit;
+}
+
+// Validación del email
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "El campo de email debe ser una dirección de correo electrónico válida.";
+    exit;
+}
+
+// Validación del teléfono
+if (preg_match('/[^0-9]+/', $telefono)) {
+    echo "El campo de teléfono solo puede contener números.";
+    exit;
+}
+
+
 
 $body = <<<HTML
     <h1>Contacto desde la web</h1>
@@ -38,5 +56,11 @@ $mailer->CharSet = 'UTF-8';
 
 $rta = $mailer->send( );
 
+if (!$mailer->send()) {
+    echo "Error al enviar el correo electrónico. Por favor, inténtalo de nuevo más tarde.";
+    exit;
+}
 //var_dump($rta);
 header("Location: index.html" );
+exit;
+?>
