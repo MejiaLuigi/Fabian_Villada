b32829ed785b8e78a6ebc87850594c227ce91e37
luis Torres <luistorresco@gmail.com>
2023-08-27 19:34:17 -0500

    correo

---
+++ funcion-mail.php
@@ -0,0 +1,46 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] != 'POST' ){
+    header("Location: index.html" );
+}
+
+/*
+if( ! isset( $_POST['nombre'] ) ){
+    header("Location: index.html" );
+}
+*/
+
+
+$nombre = $_POST['nombre'];
+
+$email = $_POST['email'];
+
+$mensaje = $_POST['mensaje'];
+
+if( empty(trim($nombre)) ) $nombre = 'anonimo';
+if( empty(trim($apellido)) ) $apellido = '';
+
+$body = <<<HTML
+    <h1>Contacto desde la web</h1>
+    <p>De: $nombre  / $email</p>
+    <h2>Mensaje</h2>
+    $mensaje
+HTML;
+
+//sintaxis de los emails email@algo.com || 
+// nombre <email@algo.com>
+
+$headers = "MIME-Version: 1.0 \r\n";
+$headers.= "Content-type: text/html; charset=utf-8 \r\n";
+$headers.= "From: $nombre $apellido <$email> \r\n";
+$headers.= "To: Sitio web <info@fabianvillada.com> \r\n";
+// $headers.= "Cc: copia@email.com \r\n";
+// $headers.= "Bcc: copia-oculta@email.com \r\n";
+
+
+//REMITENTE (NOMBRE/APELLIDO - EMAIL)
+//ASUNTO 
+//CUERPO 
+$rta = mail('info@fabianvillada.com', "Mensaje web: $asunto", $body, $headers );
+//var_dump($rta);
+
+header("Location: index.html" );
---
+++ phpmailer.php
@@ -0,0 +1,41 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] != 'POST' ){
+    header("Location: index.html" );
+}
+
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/Exception.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+$nombre = $_POST['nombre'];
+
+$email = $_POST['email'];
+$asunto = $_POST['asunto'];
+$mensaje = $_POST['mensaje'];
+$foto = $_FILES['foto']; //array assoc - $foto['tmp_name']; $foto['size'] - $foto['name']
+
+if( empty(trim($nombre)) ) $nombre = 'anonimo';
+
+
+$body = <<<HTML
+    <h1>Contacto desde la web</h1>
+    <p>De: $nombre  / $email</p>
+    <h2>Mensaje</h2>
+    $mensaje
+HTML;
+
+$mailer = new PHPMailer();
+$mailer->setFrom( $email, "$nombre " );
+$mailer->addAddress('info@fabianvillada.com','Sitio web');
+$mailer->Subject = "Mensaje web: $asunto";
+$mailer->msgHTML($body);
+$mailer->AltBody = strip_tags($body);
+$mailer->CharSet = 'UTF-8';
+
+
+
+$rta = $mailer->send( );
+
+//var_dump($rta);
+header("Location: index.html" );
